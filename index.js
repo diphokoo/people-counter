e28f8d9af104f333316d024dcabae30dfb035374
diphokoo <diphokoo@outlook.com>
2022-08-24 22:57:34 +0200

    adding the save btn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var countEl = document.getElementById("count-el")
 var count = 0
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
+
+function save() {
+    console.log(count)
+}
+ 
