a0d04e20e5602815c4e4e5c49419e3dbd43c807c
diphokoo <diphokoo@outlook.com>
2022-08-24 23:20:58 +0200

    fixing spaces

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var countEl = document.getElementById("count-el")
+var saveEl = document.getElementById("save-el")
 var count = 0
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
+
 function save() {
+    var countStr = count + "-"
+
+    saveEl.textContent += countStr
+
     console.log(count)
 }
  
